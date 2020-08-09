144eaa019e1b1201235a9f6996a5d1353f614151
Kevin N <45763760+Kevinnko@users.noreply.github.com>
2020-08-09 13:04:33 +0200

    integrate design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Background from "./assets/images/converter-bg.jpg";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="container"
+      style={{ backgroundImage: `url(${Background})` }}
+    >
+      <div className="content-wrapper">
+        <div className="left-wrapper">
+          <h1>
+            Stunning
+            <br /> €uro converter
+          </h1>
+          <p className="subtitle">
+            Convert euros in the currency of your choice in no time. It’s easy,
+            it’s free, and it’s based on real-time exchange rate for 170 world
+            currencies.
+          </p>
+          <button>Let's go</button>
+        </div>
+        <div className="converter">
+          <h2>How to convert your euros into antoher currency</h2>
+          <p className="subtitle">
+            Enter the amount of euros you want to convert, and choose the
+            currency you want. We do the conversion and serve it to you on a
+            silver platter, instantly.
+          </p>
+          <div className="form-wrapper">
+            <input
+              type="number"
+              name="euros"
+              placeholder="Enter here your amount in euros"
+            />
+            <select>
+              <option selected value="coconut">
+                Noix de coco
+              </option>
+              <option value="mango">Mangue</option>
+            </select>
+          </div>
+          <p className="result">Your result will appear here ;)</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
