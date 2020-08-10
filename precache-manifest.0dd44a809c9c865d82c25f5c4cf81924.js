853421fef3b1f02faac43d01e31bca2b72a0f88d
Kevin N <45763760+Kevinnko@users.noreply.github.com>
2020-08-10 14:27:51 +0200

    Updates

---
+++ precache-manifest.0dd44a809c9c865d82c25f5c4cf81924.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c1a1e6db2b881461db0b33fb9bd160",
+    "url": "/euro-converter/index.html"
+  },
+  {
+    "revision": "43585d8652b03ec5e8bf",
+    "url": "/euro-converter/static/css/main.00069f1e.chunk.css"
+  },
+  {
+    "revision": "8f8fffbdbaf336e886db",
+    "url": "/euro-converter/static/js/2.e277e1ec.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/euro-converter/static/js/2.e277e1ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43585d8652b03ec5e8bf",
+    "url": "/euro-converter/static/js/main.ff0bb26a.chunk.js"
+  },
+  {
+    "revision": "457c58df3fe1f8e104cd",
+    "url": "/euro-converter/static/js/runtime-main.1dfd4d31.js"
+  },
+  {
+    "revision": "415346af785091359b30bb3e0fd76799",
+    "url": "/euro-converter/static/media/converter-bg.415346af.jpg"
+  }
+]);
